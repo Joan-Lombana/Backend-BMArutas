0b4970961e01bd10fa3302157b22621e1d30d590
Joan-Lombana <joan.lombana@correounivalle.edu.co>
2025-11-26 22:26:30 -0500

    nuevas variables de entorno

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:4200',
+    origin: 'process.env.FRONTEND_URL',
     credentials: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Authorization',
