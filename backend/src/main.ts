060864a2b61c01f8d4eb6f598018c38b1754211c
Joan-Lombana <joan.lombana@correounivalle.edu.co>
2025-11-26 23:07:23 -0500

    correccion de cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'process.env.FRONTEND_URL',
+    origin: process.env.FRONTEND_URL,
     credentials: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Authorization',
--- rutas-service/src/main.ts
+++ rutas-service/src/main.ts
@@ -4,8 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors(); // ✅ permite solicitudes desde el frontend o Postman
-  await app.listen(process.env.PORT ?? 3001, '0.0.0.0'); // ✅ escucha en todas las interfaces
-  console.log(`🚀 Servidor corriendo en http://0.0.0.0:${process.env.PORT ?? 3001}`);
+  await app.listen(3001, '0.0.0.0'); // ✅ escucha en todas las interfaces
+  console.log(`🚀 Servidor corriendo en http://0.0.0.0:${3001}`);
 }
 bootstrap();
 
