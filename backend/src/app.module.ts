02226c8be3b56c69099b3e944bfcc83c52ccf4f8
Joan-Lombana <joan.lombana@correounivalle.edu.co>
2025-11-28 19:56:21 -0500

    correccion de la varible POSTGRES_PASSWORD

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -15,7 +15,7 @@ import { OperativoModule } from './operativo/operativo.module';
     host: process.env.DB_HOST,
     port: parseInt(process.env.DB_PORT ?? '5432', 10),
     username: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
+    password: String(process.env.POSTGRES_PASSWORD),
     database: process.env.POSTGRES_DB,
     autoLoadEntities: true,
     synchronize: true,
