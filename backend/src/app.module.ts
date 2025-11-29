a904383d446253e0a78f21d61f290cda02df177b
Joan-Lombana <joan.lombana@correounivalle.edu.co>
2025-11-28 20:05:09 -0500

    correccion de la varible POSTGRES_PASSWORD

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,23 +6,28 @@ import { AppService } from './app.service';
 import { AuthModule } from './autenticacion/auth.module';
 import { OperativoModule } from './operativo/operativo.module';
 
+// ✅ Logs de depuración antes de inicializar el módulo
+console.log('DB_HOST:', process.env.DB_HOST);
+console.log('DB_PORT:', process.env.DB_PORT);
+console.log('POSTGRES_USER:', process.env.POSTGRES_USER);
+console.log('POSTGRES_PASSWORD:', process.env.POSTGRES_PASSWORD);
+console.log('POSTGRES_DB:', process.env.POSTGRES_DB);
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }), // ✅ carga .env
+    ConfigModule.forRoot({ isGlobal: true }), // carga .env
     TypeOrmModule.forRoot({
-    type: 'postgres',
-    host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT ?? '5432', 10),
-    username: process.env.POSTGRES_USER,
-    password: String(process.env.POSTGRES_PASSWORD),
-    database: process.env.POSTGRES_DB,
-    autoLoadEntities: true,
-    synchronize: true,
-  }),
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT ?? '5432', 10),
+      username: process.env.POSTGRES_USER,
+      password: String(process.env.POSTGRES_PASSWORD), // 👈 fuerza a string
+      database: process.env.POSTGRES_DB,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
     AuthModule,
     OperativoModule,
-   
   ],
   controllers: [AppController],
   providers: [AppService],
