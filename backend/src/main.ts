803222b1a229d4248a9e629f7c192b66b33a6824
Joan-Lombana <joan.lombana@correounivalle.edu.co>
2025-11-30 04:21:40 -0500

    cambio en el pipeline

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as dotenv from 'dotenv';
-import * as path from 'path';
 
-
-
-dotenv.config({ path: path.resolve(__dirname, '../../../.env') });
 
 
 
