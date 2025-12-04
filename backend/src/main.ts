363b75dc961338915de1332e93d2af8cbf0a0055
Joan-Lombana <joan.lombana@correounivalle.edu.co>
2025-12-04 00:34:37 -0500

    importacion de cookie-perser

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import * as cookieParser from 'cookie-parser';
 
 
 async function bootstrap() {
@@ -14,6 +14,7 @@ console.log('POSTGRES_DB:', process.env.POSTGRES_DB);
 const app = await NestFactory.create(AppModule);
   
   app.setGlobalPrefix('api');
+  app.use(cookieParser());
   app.enableCors({
     origin: process.env.FRONTEND_URL,
     credentials: true,
