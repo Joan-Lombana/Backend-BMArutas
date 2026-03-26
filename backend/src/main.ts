2d512cf4fba9246be5cfcdb6b09586b7712a8e91
Joan-Lombana <joan.lombana@correounivalle.edu.co>
2026-03-25 22:55:28 -0500

    ajuste de los cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,24 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-
 async function bootstrap() {
 
-  // ✅ Logs de depuración antes de inicializar el módulo
-console.log('DB_HOST:', process.env.DB_HOST);
-console.log('DB_PORT:', process.env.DB_PORT);
-console.log('POSTGRES_USER:', process.env.POSTGRES_USER);
-console.log('POSTGRES_PASSWORD:', process.env.POSTGRES_PASSWORD);
-console.log('POSTGRES_DB:', process.env.POSTGRES_DB);
-const app = await NestFactory.create(AppModule);
-  
+  console.log('DB_HOST:', process.env.DB_HOST);
+  console.log('DB_PORT:', process.env.DB_PORT);
+  console.log('POSTGRES_USER:', process.env.POSTGRES_USER);
+  console.log('POSTGRES_PASSWORD:', process.env.POSTGRES_PASSWORD);
+  console.log('POSTGRES_DB:', process.env.POSTGRES_DB);
+
+  const app = await NestFactory.create(AppModule);
+
   app.setGlobalPrefix('api');
+
+  // ✅ CORS abierto para app web + app móvil
   app.enableCors({
-    origin: process.env.FRONTEND_URL,
+    origin: true,
     credentials: true,
   });
 
   await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
-  console.log(`🚀 Servidor corriendo en http://0.0.0.0:${process.env.PORT ?? 3000}`);
+
+  console.log(
+    `🚀 Servidor corriendo en http://0.0.0.0:${process.env.PORT ?? 3000}`,
+  );
 }
+
 bootstrap();
